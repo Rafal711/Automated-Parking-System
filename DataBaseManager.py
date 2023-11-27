658d381056636e9b4653cabc5f6ff35aee07ccf1
MiloszBlachowiak <mblachowiak@student.agh.edu.pl>
2023-11-27 10:54:40 +0100

    Add database manager

--- DataBaseManager.py
+++ DataBaseManager.py
@@ -1,14 +1,17 @@
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, func, desc, MetaData, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Float
+from datetime import datetime
 
-# modify this
-db_string = "postgresql://postgres:admin@localhost:5432/intelligent_parking"
+
+# ------------------------------------------------------------------------------------
+
+# modify this database address to what you have set in PgAdmin
+db_string = "postgresql://postgres:admin@localhost:5432/Parking_lot_database"
 
 engine = create_engine(db_string)
 
 Base = declarative_base()
-
 
 # ------------------------------------------------------------------------------------
 
@@ -28,4 +31,124 @@ class Parking_lot(Base):
     
 # ------------------------------------------------------------------------------------
 
-Base.metadata.create_all(engine)
+class ParkingDB:
+    def __init__(self):
+        Base.metadata.create_all(engine)
+
+        #initialize mapper operation
+        self.metadata = MetaData()
+        self.Parking_lot_table = Table(engine.table_names()[0], self.metadata , autoload=True, autoload_with=engine)
+
+
+    # Database handling functions
+    def addCarEntryRecord(self, registration):
+        ins = self.Parking_lot_table.insert().values(ID = self.getTableLength() + 1,
+                                                Registration_number = registration,
+                                                Entrance_time = datetime.now(),
+                                                Exit_time = None,
+                                                Parking_time = None,
+                                                Fee = None)
+        result = engine.execute(ins)
+
+
+    def updateParkingTime(self, id):
+        if id > self.getTableLength():
+            return None
+        
+        # add parking time in minutes
+        mapper_stmt = select(self.Parking_lot_table.columns.Entrance_time, self.Parking_lot_table.columns.Exit_time).\
+                where(self.Parking_lot_table.columns.ID == id)
+
+        result = engine.execute(mapper_stmt).fetchall()
+        minutes = (result[0][1] - result[0][0]).total_seconds() / 60
+
+        upd_parking_time = self.Parking_lot_table.update().\
+                    where(self.Parking_lot_table.columns.ID == id).\
+                    values(Parking_time = minutes)
+        engine.execute(upd_parking_time)
+
+        return minutes
+
+
+    def updateFee(self, id, minutes, price_per_hour = 2):
+        if id > self.getTableLength():
+            return None
+        
+        fee = round(minutes / 60 * price_per_hour, 2)
+
+        upd_fee = self.Parking_lot_table.update().\
+                    where(self.Parking_lot_table.columns.ID == id).\
+                    values(Fee = fee)
+        engine.execute(upd_fee)
+
+        return fee
+
+
+    def updateAtExit(self, registration):
+        id = self.findLastIdByRegistration(registration)
+
+        # update exit time
+        if id is not None:
+            upd_exit = self.Parking_lot_table.update().\
+                    where(self.Parking_lot_table.columns.ID == id).\
+                    values(Exit_time = datetime.now())
+
+            engine.execute(upd_exit)
+        else:
+            print("W bazie nie istnieje dany numer")
+            return None
+        
+        minutes = self.updateParkingTime(id)
+        self.updateFee(id, minutes)
+
+
+    def getTableLength(self):
+        count_stmt = select(func.count(self.Parking_lot_table.columns.ID))
+        return engine.execute(count_stmt).fetchall()[0][0]
+
+
+    def findLastIdByRegistration(self, registration):
+        mapper_stmt = select(self.Parking_lot_table.columns.ID).\
+                    where(self.Parking_lot_table.columns.Registration_number == registration).\
+                    order_by(desc(self.Parking_lot_table.columns.ID)).\
+                    limit(1)
+        
+        result = engine.execute(mapper_stmt).fetchall()
+
+        if result:
+            return result[0][0]
+        else:
+            return None
+
+
+    def getFee(self, registration):
+        id = self.findLastIdByRegistration(registration)
+
+        mapper_stmt = select(self.Parking_lot_table.columns.Fee).\
+                    where(self.Parking_lot_table.columns.ID == id)
+        
+        result = engine.execute(mapper_stmt).fetchall()
+
+        if result:
+            return result[0][0]
+        else:
+            return None
+
+
+    def getParkingTimeInMinutes(self, registration):
+        id = self.findLastIdByRegistration(registration)
+
+        mapper_stmt = select(self.Parking_lot_table.columns.Parking_time).\
+                    where(self.Parking_lot_table.columns.ID == id)
+        
+        result =  engine.execute(mapper_stmt).fetchall()[0][0]
+
+        if result:
+            return result[0][0]
+        else:
+            return None
+
+
+    def getParkingTimeInHours(self, registration):
+        minutes = self.getParkingTimeInMinutes(registration)
+        return round(minutes / 60, 2)
